266a56c2969beb8443ebe710f16740c8dd63e388
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2019-04-02 14:23:41 -0400

    Fixed radix n added a getDigit method.

--- Radix.java
+++ Radix.java
@@ -1,18 +1,29 @@
+
 public class Radix {
   private int index;
 
  public static void radixsort(int[]data){
    //to sort this, try comparing from the first digit and then slowly move up towards the last digit.
-   index = data.length;
-   Node<int> temp;
+    @SuppressWarnings("unchecked")
+   MyLinkedList<Integer> buckets = new MyLinkedList[20];
+   int max = data[0];
+   for (int i = 0; i < buckets.length; i++) {
+     buckets[i] = new MyLinkedList();
+   }
 
-   for (int i = 0; i < index; i++){
-     int number;
-     //number = digit(data[i]);
-     if (data[i] < 10 || data[i] >= 0){
-       number = data[i];
-       number.add(index - i);
-     }
+    for (int i = 0; i < data.length; i++) {
+      if (max < data[i]) {
+      max = data[i];
+    }
+  }
+
+  for (int i = 0; i < index; i++){
+    int number;
+    if (data[i] < 10 || data[i] >= 0){
+      number = data[i];
+
+    }
+  }
 
      if (data[i] >= 10 || data[i] < 100) {
        number = data[i] % 10;
@@ -31,18 +42,17 @@ public class Radix {
 
  }
 
- public int getValue() {
-
- }
-
- public int getDigit(int n){
-
-
- }
+ public static int getDigit(int i, int num) {
+    while (num > Math.pow(10, i)) {
+      num = num / 10;
+    }
+    return num % 10;
+  }
 
 
+public static void main(String[] args) {
 
-
+}
 
 
 
