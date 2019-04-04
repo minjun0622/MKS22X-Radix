c3b6b3ceb2a508774e1cdde78bfec64c246fb0ed
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2019-04-04 01:07:55 -0400

    Added node to linkedlist

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,4 +1,50 @@
+import java.io.* ;
+import java.util.* ;
+
 public class MyLinkedList<E>{
+
+  public class Node {
+    private E data ;
+    private Node next, prev ;
+    public Node(E d, Node p, Node n) {
+      data = d ;
+      next = n ;
+      prev = p ;
+    }
+    public Node(E d) {
+      data = d ;
+      prev = null ;
+      next = null ;
+    }
+    public E getData() {
+      return data ;
+    }
+    public Node next() {
+      return next ;
+    }
+    public Node prev() {
+      return prev ;
+    }
+    public boolean hasNext() {
+      if (next == null) return false ;
+      return true ;
+    }
+    public E setData(E d) {
+      data = d ;
+      return data ;
+    }
+    public void setNext(Node n) {
+      next = n ;
+    }
+    public void setPrev(Node p) {
+      prev = p ;
+    }
+    public String toString() {
+      String fin = "" ;
+      return fin ;
+    }
+  }
+  
   private int size;
   private Node<E> start;
   private Node<E> end;
@@ -158,6 +204,7 @@ public class MyLinkedList<E>{
     }
     return false;
   }
+   @SuppressWarnings("unchecked")
   public void extend(MyLinkedList<E> other) {
     if (this.size == 0) {
       this.start = other.start;
